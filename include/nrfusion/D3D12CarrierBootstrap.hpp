ee4e92f1e4eadd7c5538123a7b1c07cf36b471a5
Thiago F. <tcfialho@gmail.com>
2026-09-23 02:52:13 -0300

    feat: define D3D12 executor deactivation

--- include/nrfusion/D3D12CarrierBootstrap.hpp
+++ include/nrfusion/D3D12CarrierBootstrap.hpp
@@ -16,7 +16,8 @@ enum class D3D12CarrierActivationFailure : std::uint8_t {
     RuntimeNotRunning,
     ProviderMissing,
     QualificationFailed,
-    ComponentActivationFailed
+    ComponentActivationFailed,
+    ComponentDeactivationFailed
 };
 
 struct D3D12CarrierActivationResult {
@@ -32,6 +33,8 @@ RuntimeBootstrapResult StartD3D12CarrierRuntime(
     RuntimeShell& shell, const RuntimeConfig& config) noexcept;
 
 D3D12CarrierActivationResult ActivateD3D12CarrierExecutor(
+    RuntimeShell& shell, const D3D12CarrierExecutorHooks& hooks) noexcept;
+D3D12CarrierActivationResult DeactivateD3D12CarrierExecutor(
     RuntimeShell& shell, const D3D12CarrierExecutorHooks& hooks) noexcept;
 
 } // namespace nrfusion
