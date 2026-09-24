445ad6dde8fdb3002b8fceecb06f05eda44b52e4
Thiago F. <tcfialho@gmail.com>
2026-09-23 23:45:39 -0300

    fix: isolate Vulkan Windows macros from contracts

--- include/nrfusion/VulkanNativeContext.hpp
+++ include/nrfusion/VulkanNativeContext.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
 #ifndef VK_USE_PLATFORM_WIN32_KHR
 #define VK_USE_PLATFORM_WIN32_KHR
 #endif
